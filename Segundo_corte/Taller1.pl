a7bad5ffb874e54cfcb2f224b7bb04c384860aaf
JulRiv07 <Julian.rivera@utp.edu.co>
2025-10-04 02:22:18 -0500

    Taller segundo corte, inicio

---
+++ Segundo_corte/Taller1.pl
@@ -0,0 +1,31 @@
+%! estructura(Padre, Madre, [Hijos]).
+familia(persona(abraham, hombre), persona(mona, mujer), [persona(herbert, hombre), persona(homero, hombre)]).
+familia(persona(clancy, hombre), persona(jacqueline, mujer), [persona(marge, mujer), persona(patty, mujer), persona(selma, mujer)]).
+familia(persona(homero, hombre), persona(marge, mujer), [persona(bart, hombre), persona(lisa, mujer), persona(maggie, mujer)]).
+familia(_,persona(selma, mujer),[persona(ling, mujer)]).
+
+padre(P, H):-
+    familia(persona(P, hombre), _, Hijos),
+    member(persona(H, _), Hijos).
+
+madre(M, H):-
+    familia(_, persona(M, mujer), Hijos), 
+    member(persona(H, _), Hijos).
+
+progenitor(P, H):-
+    madre(P, H) , padre(P, H).
+
+hijo(H, P):-
+    (familia(persona(P, hombre), _, Hijos) ; familia(_, persona(P, mujer), Hijos)),
+    member(persona(H, hombre), Hijos).
+
+hija(H, P):-
+    (familia(persona(P, hombre), _, Hijos) ; familia(_, persona(P, mujer), Hijos)),
+    member(persona(H, mujer), Hijos).
+
+hermanos(A, B):-
+    A \= B,
+    progenitor(P, A), 
+    progenitor(P, B).
+
+abuelo(A , N):-
