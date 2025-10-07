adc867aedfe8b456b98c2716c1889c7e9243d178
JulRiv07 <Julian.rivera@utp.edu.co>
2025-10-07 10:06:16 -0500

    Inicio Python

--- Python/D1.py
+++ Python/D1.py
@@ -1,1 +1,8 @@
 print("Hello world")
+name = "Julian"
+if len(name) < 5:
+    print("Nombre corto")
+elif len(name) > 5 & len(name) < 10:
+    print("Nombre largo")
+else : 
+    print("Nombre gigante")
